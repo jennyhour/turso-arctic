b08f82eacfb4147a752e297065625382f79868fb
Newton Ni <nwtnni@gmail.com>
2026-04-30 21:50:55 +0000

    Update benchmark script

--- perf/throughput/turso/scripts/bench.sh
+++ perf/throughput/turso/scripts/bench.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-cargo build --release
+# cargo build --release
 
-echo "system,threads,batch_size,compute,throughput"
+echo "index,system,threads,batch_size,compute,throughput" >> out.log
 
-for threads in 1 2 4 8; do
-  for compute in 0 100 500 1000; do
-      rm -f write_throughput_test.db*
-      ../../../target/release/write-throughput --threads ${threads} --batch-size 100 --compute ${compute} -i 1000 --mode concurrent
-  done
+for iteration in $(seq 10); do
+    for threads in 1 2 4 8; do
+        for index in arctic skipmap; do
+            rm -f write_throughput_test.db*
+
+            echo -n "$index," >> out.log
+
+            "./write-throughput-row-$index" --threads $threads --batch-size 100 --compute 0 -i $((16000 / $threads)) --mode concurrent >> out.log
+        done
+    done
 done
